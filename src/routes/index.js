c995e2f7564cb43a8cead08fb799d5b8b5a50695
Bùi Hữu Lộc <feature451@gmail.com>
2022-07-18 21:02:00 +0700

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,10 @@ import Login from '~/pages/site/Login';
 import MngRegisterForm from '~/pages/manage/RegisterForm';
 import MngContract from '~/pages/manage/Contract';
 import MngMistake from '~/pages/manage/Mistake';
+import MngForm from '~/pages/manage/Form';
+import MngFormDetail from '~/pages/manage/FormDetail';
 import StdContract from '~/pages/student/Contract';
+import StdMistake from '~/pages/student/Mistake';
 import Test from '~/pages/Test';
 
 export const routes = [
@@ -28,8 +31,20 @@ export const routes = [
     'Component': MngMistake
   },
   { 
+    'path': '/quan-ly/don-khieu-nai',
+    'Component': MngForm
+  },
+  { 
+    'path': '/quan-ly/don-khieu-nai/:id',
+    'Component': MngFormDetail
+  },
+  { 
     'path': '/sinh-vien/hop-dong',
     'Component': StdContract
+  },
+  { 
+    'path': '/sinh-vien/vi-pham',
+    'Component': StdMistake
   },
   { 
     'path': '/test',
