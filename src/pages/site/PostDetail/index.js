d7a14b2da0aebcc626424c1cd099c80f2ec898a9
OhJiKang <saofaixoan5464@gmail.com>
2022-07-26 11:53:13 +0700

    Update index.js
    50% done

--- src/pages/site/PostDetail/index.js
+++ src/pages/site/PostDetail/index.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 
 import MyNavbar from "~/components/MyNavbar";
 import MyFooter from "~/components/MyFooter";
-import { useGetPost } from './hooks';
+import { useGetPost } from "./hooks";
 
 function PostDetail() {
   console.log("Page: PostDetail");
@@ -20,40 +20,87 @@ function PostDetail() {
         onSuccess(data) {
           console.log(data.data);
           setPost(data.data);
-        }
+        },
       }
-    )
+    );
   }, []);
 
   return (
     <>
       <MyNavbar isSite={true}></MyNavbar>
 
-      {!post 
-        ? <>Loading...</> 
-        : (
-          <>
-            <div>
-              <img
-                src={post.post.banner}
-                alt=""
-                style={{ width: "395px", height: "206px" }}
-              />
-              <div style={{ margin: '12px 0px' }}>{post.post.title}</div>
-              <div style={{ margin: '12px 0px' }}>{post.post.category}</div>
-              <div style={{ margin: '12px 0px' }}>{post.post.created_at}</div>
-              <div style={{ margin: '12px 0px' }} dangerouslySetInnerHTML={{ __html: post.post.content }}></div>
-              <div style={{ margin: '12px 0px' }}>
-                {post.tags.map(({ id, name, color }, index) => (
-                  <div style={{ color: color }} key={id + '_' + index}>{name}</div>
-                ))}
+      {!post ? (
+        <>Loading...</>
+      ) : (
+        <>
+          <div style={{ margin: "20px 115px", display: "flex" }}>
+            <div className="post_detail" style={{ width: "50%" }}>
+              <div
+                style={{
+                  fontWeight: "700",
+                  fontSize: "24px",
+                  lineHeight: "29px",
+                }}
+              >
+                {post.post.title}
+              </div>
+              <div style={{ margin: "12px 0px" }}>{post.post.created_at}</div>
+              <div
+                style={{ margin: "12px 0px" }}
+                dangerouslySetInnerHTML={{ __html: post.post.content }}
+              ></div>
+            </div>
+            <div style={{ margin: "0 10%" }} className="relativepost">
+              {/* {post.tags.map(({ id, name, color }, index) => (
+                <div style={{ color: color }} key={id + "_" + index}>
+                  {name}
+                </div>
+              ))} */}
+              <div className="category">
+                <div
+                  className="category-heading"
+                  style={{
+                    borderBottom: "#0B42AB solid 3px",
+                    width: "100%",
+                  }}
+                >
+                  <h1
+                    style={{
+                      fontSize: "24px",
+                    }}
+                  >
+                    Thể loại
+                  </h1>
+                </div>
+                <div className="category-content" style={{ margin: "12px 0" }}>
+                  <div
+                    style={{
+                      margin: "0 12px",
+                      height: "39px",
+                      backgroundColor: "#A9CBFE",
+                      padding: "10px",
+                    }}
+                  >
+                    <p
+                      style={{
+                        color: "#0B42AB",
+                        margin: "0",
+                        fontSize: "16px",
+                        height: "19px",
+                        lineHeight: "19px",
+                      }}
+                    >
+                      {post.post.category}
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
-      
-            <MyFooter></MyFooter>
-          </>
-        )
-      }
+          </div>
+
+          <MyFooter></MyFooter>
+        </>
+      )}
     </>
   );
 }
