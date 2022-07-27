3fc41b62383a721765bb6aedc393bf99290fd079
OhJiKang <saofaixoan5464@gmail.com>
2022-07-27 16:11:02 +0700

    Final Up

--- src/pages/site/PostDetail/index.js
+++ src/pages/site/PostDetail/index.js
@@ -31,7 +31,36 @@ function PostDetail() {
 
   const [post, setPost] = useState(null);
   const [postsRelated, setPostsRelated] = useState(null);
-
+  function padZero(str, len) {
+    len = len || 2;
+    var zeros = new Array(len).join("0");
+    return (zeros + str).slice(-len);
+  }
+  function invertColor(hex, bw) {
+    if (hex.indexOf("#") === 0) {
+      hex = hex.slice(1);
+    }
+    // convert 3-digit hex to 6-digits.
+    if (hex.length === 3) {
+      hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+    }
+    if (hex.length !== 6) {
+      throw new Error("Invalid HEX color.");
+    }
+    var r = parseInt(hex.slice(0, 2), 16),
+      g = parseInt(hex.slice(2, 4), 16),
+      b = parseInt(hex.slice(4, 6), 16);
+    if (bw) {
+      // https://stackoverflow.com/a/3943023/112731
+      return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? "#000000" : "#FFFFFF";
+    }
+    // invert color components
+    r = (255 - r).toString(16);
+    g = (255 - g).toString(16);
+    b = (255 - b).toString(16);
+    // pad each with zeros and return
+    return "#" + padZero(r) + padZero(g) + padZero(b);
+  }
   useEffect(() => {
     getPost.mutate(
       { id },
@@ -64,7 +93,7 @@ function PostDetail() {
       ) : (
         <>
           <div style={{ margin: "20px 115px", display: "flex" }}>
-            <div className="post_detail" style={{ width: "50%" }}>
+            <div className="post_detail" style={{ width: "70%" }}>
               <div
                 style={{
                   fontWeight: "700",
@@ -212,9 +241,10 @@ function PostDetail() {
                           style={{
                             backgroundColor: color,
                             padding: "10.5px 12px",
-                            color: "#FFFFFF",
+                            color: invertColor(color, "#FFFFFF"),
                             fontSize: "16px",
                             lineHeight: "19px",
+                            fontWeight: "700",
                           }}
                         >
                           {name}
