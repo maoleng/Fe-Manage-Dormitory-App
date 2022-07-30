39b21d0506e1d67bfc12518736b98766760223df
Krotohmaki <tri.pm.1405@gmail.com>
2022-07-30 20:34:59 +0700

    not

--- src/pages/manage/ElectricityWaters/hooks/index.js
+++ src/pages/manage/ElectricityWaters/hooks/index.js
@@ -1,6 +1,107 @@
 import axios from "axios";
 import { useMutation } from "react-query";
 
+export const useGetBuildings = () => {
+  const mutation = useMutation(async () => {
+    const { data } = await axios.get(
+      process.env.REACT_APP_API_ENDPOINT + '/mng/building', 
+      {
+        'headers': {
+          'Authorization': 'Bearer ' + window.localStorage.getItem('token'),
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+
+    return data;
+  });
+
+  return mutation;
+}
+
+export const useGetFloors = () => {
+  const mutation = useMutation(async () => {
+    const { data } = await axios.get(
+      process.env.REACT_APP_API_ENDPOINT + '/mng/floor', 
+      {
+        'headers': {
+          'Authorization': 'Bearer ' + window.localStorage.getItem('token'),
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+
+    return data;
+  });
+
+  return mutation;
+}
+
+export const useGetYears = () => {
+  const mutation = useMutation(async () => {
+    const { data } = await axios.get(
+      process.env.REACT_APP_API_ENDPOINT + '/mng/subscription/year_range/hop_dong', 
+      {
+        'headers': {
+          'Authorization': 'Bearer ' + window.localStorage.getItem('token'),
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+
+    return data;
+  });
+
+  return mutation;
+}
+
+export const usePutBill = () => {
+  const mutation = useMutation(async ({ body, id }) => {
+    console.log('API:', process.env.REACT_APP_API_ENDPOINT + `/mng/subscription/${id}`);
+    console.log('Body:', body);
+    const { data } = await axios.put(
+      process.env.REACT_APP_API_ENDPOINT + `/mng/subscription/${id}`,
+      body,
+      {
+        'headers': {
+          'Authorization': 'Bearer ' + window.localStorage.getItem('token'),
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+
+    return data;
+  });
+
+  return mutation;
+}
+
+export const usePostDownload = () => {
+  const mutation = useMutation(async ({ body }) => {
+    console.log('body:', body);
+    const { data } = await axios.post(
+      process.env.REACT_APP_API_ENDPOINT + `/mng/subscription/download`,
+      body,
+      {
+        'headers': {
+          'Authorization': 'Bearer ' + window.localStorage.getItem('token'),
+          'Content-Type': 'application/json'
+        },
+        'redirect': 'follow', // manual, *follow, error
+        'referrerPolicy': 'no-referrer',
+        'mode': 'cors', // no-cors, *cors, same-origin
+        'cache': 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        'credentials': 'same-origin',
+        'responseType': 'blob'
+      }
+    );
+
+    return data;
+  });
+
+  return mutation;
+}
+
 export const useGetElectricityWater = () => {
   const mutation = useMutation(async ({ id }) => {
     const { data } = await axios.get(
@@ -22,12 +123,14 @@ export const useGetElectricityWater = () => {
 export const useGetElectricityWaters = () => {
   const mutation = useMutation(async ({ buildingID, floorID, year, month, isPaid }) => {
     const params = [
-      ...(!buildingID ? [] : [`building_id=${buildingID}`]), 
-      ...(!floorID ? [] : [`floor_id=${floorID}`]), 
-      ...(!year ? [] : [`year=${year}`]), 
-      ...(!month ? [] : [`month=${month}`]), 
-      ...(!isPaid ? [] : [`is_paid=${isPaid}`]), 
+      ...(buildingID === undefined ? [] : [`building_id=${buildingID}`]), 
+      ...(floorID === undefined ? [] : [`floor_id=${floorID}`]), 
+      ...(year === undefined ? [] : [`year=${year}`]), 
+      ...(month === undefined ? [] : [`month=${month}`]), 
+      ...(isPaid === undefined ? [] : [`is_paid=${isPaid}`]), 
     ].join('&');
+
+    console.log(process.env.REACT_APP_API_ENDPOINT + `/mng/electricity_water${params && `?${params}`}`);
 
     const { data } = await axios.get(
       process.env.REACT_APP_API_ENDPOINT + `/mng/electricity_water${params && `?${params}`}`, 
