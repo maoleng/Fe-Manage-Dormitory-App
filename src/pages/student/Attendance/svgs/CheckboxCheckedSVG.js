a6f1cbf6db3d59780df8063334cc1472f6c0115b
Bùi Hữu Lộc <feature451@gmail.com>
2022-08-03 03:37:37 +0700

    fix

---
+++ src/pages/student/Attendance/svgs/CheckboxCheckedSVG.js
@@ -0,0 +1,10 @@
+function CheckboxCheckedSVG({ style }) {
+  return (
+    <svg style={style} viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect width="8" height="8" rx="2" fill="#35D66C"/>
+      <path d="M3.42876 5.71484L1.71436 3.9648L2.20409 3.46489L3.42928 4.71414L3.42876 4.71467L6.36748 1.71484L6.85721 2.21476L3.91849 5.21493L3.42876 5.71484Z" fill="white"/>
+    </svg>
+  );
+}
+
+export default CheckboxCheckedSVG;
